1b8bc6248b1c4a0b401ddfee6679f9cab4824b43
MilesBradt <smbradtmichael@gmail.com>
2018-08-17 08:47:27 -0700

    take user input and place into an arrary

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,29 @@
 // Numbers that contain a 1 are replaced (all digits) with "Boop!"
 // Numbers divisible by 3 are replaced with "I'm sorry, Dave. I'm afraid I can't do that."
 
+// Ideas
+// Convert array into to string
+// Run for loop
+// Pop all spec numbers
+// Clone array then ??????
+
 $(document).ready(function() {
-  for (i = 0; i < boop.length; i ++) {
-    var beepBoop = boop[i];
-    if (boop === "0") {
-      alert("boop");
-    }
-  }
+  $("form#userNumbers").submit(function(event){
+    event.preventDefault();
+    var boops = [];
+
+    $("input#userInput").each(function(){
+      var userInput = $(this).val();
+      boops.push(userInput);
+
+    });
+
+    // for (i = 0; i < boops.length; i ++) {
+    //   var beepBoop = boops[i];
+    //   if (boops === "0") {
+    //     alert("boop");
+    //   }
+    // }
+    console.log(boops);
+  });
 });
