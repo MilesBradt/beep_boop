acb8a28fd5abd5a6e6c71b8455442faae118cf55
MilesBradt <smbradtmichael@gmail.com>
2018-08-17 16:01:26 -0700

    attempt to add fist spec, only to not have it work

--- js/scripts.js
+++ js/scripts.js
@@ -17,52 +17,62 @@ var increments = [];
 $(document).ready(function() {
   $("form#userNumbers").submit(function(event){
     event.preventDefault();
-    var input = parseInt($("#userInput").val());
-    if (ranges <= input) {
-      increments.push(ranges ++);
-    }
+    // var input = parseInt($("#userInput").val());
+    parseInt($("input#userInput").each(function(){
+      var input = $(this).val();
+      if (ranges <= input) {
+        increments.push(ranges ++);
+      };
 
-    // Doesn't work at all
-    // ranges.forEach(function(range){
-    //   if (range < input) {
-    //     ranges ++;
-    //   }
-    // });
+      // Spec attempt (doesn't work)
+      results = increments.toString().split("");
+      for (var i = 0; i < results.length; i ++) {
+        results = results[i].replace(/0/g, 'beep')
+      };
 
-    // Increments by 1 but never stops and only during each click
-    // ranges = ranges.map(function(val){
-    //   return ++ val;
-
-    // Only increments once then breaks and only during each click
-    // ranges.forEach(function(range) {
-    //
-    //
-    //   //Spec attempts (none work)
-    //   // for (var i = 0; i < increments.length; i ++) {
-    //   //   // Only does bottom one
-    //   //   // number = increments[i].replace(/1/g, 'boop');
-    //   //   // number = increments[i].replace(/0/g, 'beep');
-    //   //
-    //   //   // Only outputs 0 for some reason
-    //   //   // if (increments[i] = 0){
-    //   //   //   increments[i] = "beep";
-    //   //   // }
-    //   //
-    //   //   // Will display "I can't do that" but only if one number is in the input
-    //   //   if (increments % 3 === 0){
-    //   //     alert("I can't do that");
-    //   //   }
-    //   // }
-    //   // console.log(increments);
-    // });
-
-    $("#output").text(increments);
-    console.log(ranges);
-    console.log(increments);
-    console.log(input);
+      $("#output").text(increments);
 
 
 
+      // Doesn't work at all
+      // ranges.forEach(function(range){
+      //   if (range < input) {
+      //     ranges ++;
+      //   }
+      // });
+
+      // Increments by 1 but never stops and only during each click
+      // ranges = ranges.map(function(val){
+      //   return ++ val;
+
+      // Only increments once then breaks and only during each click
+      // ranges.forEach(function(range) {
+
+
+      //Spec attempts (none work)
+      // for (var i = 0; i < increments.length; i ++) {
+      // Only does bottom one
+      // number = increments[i].replace(/1/g, 'boop');
+      // number = increments[i].replace(/0/g, 'beep');
+
+      // Only outputs 0 for some reason
+      // if (increments[i] = 0){
+      //   increments[i] = "beep";
+      // }
+      //
+      // Will display "I can't do that" but only if one number is in the input
+      //   if (increments % 3 === 0){
+      //     alert("I can't do that");
+      //   }
+      // }
+      // console.log(increments);
+      // });
+      console.log(ranges);
+      console.log(increments);
+
+
+
+
+    }));
   });
-  
 });
