c49dcc5e5434d66408daf0f840ed4748c39193cf
MilesBradt <smbradtmichael@gmail.com>
2018-08-17 15:32:04 -0700

    increments arrary by 1 and stops by user input value each click

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 // Specs
-// Numbers that contain a 0 are replaced (all digits) with "Beep!"
-// Numbers that contain a 1 are replaced (all digits) with "Boop!"
-// Numbers divisible by 3 are replaced with "I'm sorry, Dave. I'm afraid I can't do that."
+// increments that contain a 0 are replaced (all digits) with "Beep!"
+// increments that contain a 1 are replaced (all digits) with "Boop!"
+// increments divisible by 3 are replaced with "I'm sorry, Dave. I'm afraid I can't do that."
 
 // Ideas
+// Get ranges in an array working first
 // Convert array into to string
-// Pop all spec numbers
+// Pop all spec increments
 // Clone array then ??????
-// Set range to display and stop a user user's input
-//
+
 
 var ranges = [0];
 var increments = [];
@@ -18,50 +18,51 @@ $(document).ready(function() {
   $("form#userNumbers").submit(function(event){
     event.preventDefault();
     var input = parseInt($("#userInput").val());
+    if (ranges <= input) {
+      increments.push(ranges ++);
+    }
 
+    // Doesn't work at all
     // ranges.forEach(function(range){
     //   if (range < input) {
     //     ranges ++;
     //   }
     // });
 
+    // Increments by 1 but never stops and only during each click
     // ranges = ranges.map(function(val){
     //   return ++ val;
 
-    ranges.forEach(function(range) {
-      if (ranges < input) {
-        increments.push(ranges ++);
-      } 
-
-
-    });
-
-    $("#output").text(increments);
-    // ranges.forEach(function(range){
-    //   increments.push(range += 1);
-    //   $("#output").text(increments);
+    // Only increments once then breaks and only during each click
+    // ranges.forEach(function(range) {
+    //
+    //
+    //   //Spec attempts (none work)
+    //   // for (var i = 0; i < increments.length; i ++) {
+    //   //   // Only does bottom one
+    //   //   // number = increments[i].replace(/1/g, 'boop');
+    //   //   // number = increments[i].replace(/0/g, 'beep');
+    //   //
+    //   //   // Only outputs 0 for some reason
+    //   //   // if (increments[i] = 0){
+    //   //   //   increments[i] = "beep";
+    //   //   // }
+    //   //
+    //   //   // Will display "I can't do that" but only if one number is in the input
+    //   //   if (increments % 3 === 0){
+    //   //     alert("I can't do that");
+    //   //   }
+    //   // }
+    //   // console.log(increments);
     // });
 
+    $("#output").text(increments);
     console.log(ranges);
+    console.log(increments);
+    console.log(input);
 
-    // for (var i = 0; i < numbers.length; i ++) {
-    //   // Only does bottom one
-    //   // number = numbers[i].replace(/1/g, 'boop');
-    //   // number = numbers[i].replace(/0/g, 'beep');
-    //
-    //   // Only outputs 0 for some reason
-    //   // if (numbers[i] = 0){
-    //   //   numbers[i] = "beep";
-    //   // }
-    //
-    //   // Will display "I can't do that" but only if one number is in the input
-    //   if (numbers % 3 === 0){
-    //     alert("I can't do that");
-    //   }
-    // }
-    // console.log(numbers);
 
 
   });
-
+  
 });
