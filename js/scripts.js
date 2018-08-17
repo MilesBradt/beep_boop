ee1e09f0e5543fc83c65f4c04944d73521f3d74e
MilesBradt <smbradtmichael@gmail.com>
2018-08-17 08:25:27 -0700

    create for loop foundation of the first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
+// Specs
+// Numbers that contain a 0 are replaced (all digits) with "Beep!"
+// Numbers that contain a 1 are replaced (all digits) with "Boop!"
+// Numbers divisible by 3 are replaced with "I'm sorry, Dave. I'm afraid I can't do that."
 
+$(document).ready(function() {
+  for (i = 0; i < boop.length; i ++) {
+    var beepBoop = boop[i];
+    if (boop === "0") {
+      alert("boop");
+    }
+  }
 });
