21b7f32ca690507930ccafcdbff5711ea6484a76
MilesBradt <smbradtmichael@gmail.com>
2018-08-19 14:50:13 -0700

    take user input and set output array

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,18 @@
 // increments that contain a 1 are replaced (all digits) with "Boop!"
 // increments divisible by 3 are replaced with "I'm sorry, Dave. I'm afraid I can't do that."
 
+var input = input;
+var output = [0];
 
 
 $(document).ready(function() {
 
-  $("button#user").click(function(event){
+  $("form#userNumbers").submit(function(event){
     event.preventDefault();
     var input = parseInt($("#userInput").val());
 
     $('.panel').show();
-    $("#output").text(results);
+    $("#output").text(output);
 
   });
 });
